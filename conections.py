9622fd7ac600b82d2e50cde8c9a21eaadca8f081
Ricardo Arias <reariast128@gmail.com>
2023-11-24 16:16:13 -0500

    Now, this have better form. There are many changes, buts this looks so much cleaner (I hope).

--- conections.py
+++ conections.py
@@ -3,7 +3,8 @@ import googleapiclient.discovery
 import requests
 from transformers import pipeline
 from tqdm import tqdm
-from utils import ReadApiKeys, Cache
+from utils import ReadApiKeys, Cache # just for debug things
+from abc import ABC, abstractmethod
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
@@ -15,7 +16,9 @@ class SentimentAnalyzer:
         )
 
     def get_sentiment_from_text(self, text: str) -> str:
-        '''Uses lxyuan/distilbert-base-multilingual-cased-sentiments-student to get the perception of a text. This can be ``positive``, ``neutral`` or ``negative``
+        '''Uses lxyuan/distilbert-base-multilingual-cased-sentiments-student 
+        to get the perception of a text. 
+        This can be ``positive``, ``neutral`` or ``negative``
         Returns the label of the text's principal sentiment.'''
 
         # pass the text to the model to get a response
@@ -24,38 +27,57 @@ class SentimentAnalyzer:
         # return the label of the top 1 sentiment
         return response[0][0]['label']
 
-
-class YoutubeConnection:
-    '''ApiHandle contains necessary functions to connect with YouTube and Hugging Face API.
-    which are used in analysis functions in this exercise.
+# Maybe can create an dataclass that represents Video Data?
+class YoutubeConnection(ABC):
+    '''YoutubeConnection contains the necessary functions to connect with YouTube API.
     
     YOUTUBE_API_KEY: YouTube API key.
     '''
 
-    def __init__(self, YOUTUBE_API_KEY: str) -> None:
+    def __init__(self, YOUTUBE_API_KEY: str, video_id: str) -> None:
         self.YOUTUBE_API_KEY = YOUTUBE_API_KEY
+        self.video_id = video_id
 
-    def fetch_youtube_video_comments(self, video_ID: str, page_token: str = '') -> dict:
-        '''Get the comments JSON of a YouTube video using googleapiclient.
-        `page_token` is an optional argument, that indicates to request to fetch only a comments page from the whole comments avaiable in the video.
-        '''
+    def fetch_data(self):
+        pass
 
-        # build the youtube conection
-        youtube_conection = googleapiclient.discovery.build(
+    def build_connection(self):
+        pass
+
+   
+
+class YoutubeCommentsConnection(YoutubeConnection):
+
+    def __init__(self, YOUTUBE_API_KEY: str, video_id: str) -> None:
+        super().__init__(YOUTUBE_API_KEY, video_id)
+
+    def build_connection(self, page_token: str = ''):
+        # build the youtube connection
+        youtube_connection = googleapiclient.discovery.build(
             'youtube', 'v3', developerKey = self.YOUTUBE_API_KEY)
         
-        request = youtube_conection.commentThreads().list(
+        request = youtube_connection.commentThreads().list(
                 part="snippet,replies",
-                videoId=video_ID,
+                videoId=self.video_id,
                 maxResults=100,
                 pageToken=page_token
-            ) # type: ignore
+            )
+        
+        return request
+
+    def fetch_data(self, page_token: str = '') -> dict:
+        '''Get the comments JSON of a YouTube video using googleapiclient.
+        `page_token` is an optional argument, that indicates to request 
+        to fetch only a comments page from the whole comments avaiable in the video.
+        '''
+
+        request = self.build_youtube_connection(page_token=page_token)
             
         # trying request and handling with possible errors
         try:
             response = request.execute() # type: ignore
             if 'error' in response.keys():
-                raise
+                raise requests.HTTPError
 
             else:
                 print(f"Request id {response['etag']} was executed sucessfully.")
@@ -66,30 +88,17 @@ class YoutubeConnection:
             print(f"Reason: {error.reason}")
             raise
 
-    def fetch_youtube_video_info(self, video_ID: str) -> dict:
-        '''Get the JSON with the video info by a HTTPS request.'''
-        request_url = f"https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id={video_ID}&key={self.YOUTUBE_API_KEY}"
-        try:
-            response = requests.get(request_url)
-            if response.status_code == 200:
-                return response.json()
-            else:
-                raise
-        except requests.HTTPError as error:
-            print(error.response)
-            raise
-
     def fetch_all_youtube_video_comments(self, video_ID: str) -> list:
-        '''Retrieves a JSON with all the comments page from a video.'''
+        '''Retrieves a JSON with all the comments page from a video using fetch_youtube_video_comments function.'''
         responses = []
         try:
             print(f"Trying to get all comments from {video_ID} video.")
-            response = self.fetch_youtube_video_comments(video_ID)
+            response = self.fetch_data(video_ID)
             responses.append(response)
 
             while 'nextPageToken' in response.keys():
                 next_page_token = response['nextPageToken']
-                response = self.fetch_youtube_video_comments(video_ID, page_token=next_page_token)
+                response = self.fetch_data(video_ID, page_token=next_page_token)
                 responses.append(response)
 
             return responses
@@ -101,31 +110,119 @@ class YoutubeConnection:
             raise
 
 
+
+class YoutubeVideoInfoConnection(YoutubeConnection):
+     
+    def __init__(self, YOUTUBE_API_KEY: str, video_id: str) -> None:
+        super().__init__(YOUTUBE_API_KEY, video_id)
+
+    def build_connection(self):
+        return f"https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id={self.video_ID}&key={self.YOUTUBE_API_KEY}"
+
+    def fetch_data(self) -> dict:
+        '''Get the JSON with the video info using a HTTPS request.'''
+        request_url = self.build_connection()
+        try:
+            response = requests.get(request_url)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                raise requests.HTTPError
+        except requests.HTTPError:
+            raise 
+
+class Cleaner(ABC):
+
+    def check_is_clean_is_needed(self, data):
+        pass
+
+    def clean_data(self):
+        pass
+
+# Maybe can turn this into an abstract class?
+class CommentsListLengthCleaner(Cleaner):
+
+    def check_is_clean_is_needed(self, comments_data: list[dict[str, str]]) -> bool:
+        return True if len(comments_data) >= 10000 else False
     
-class DataCleaner:
+    def clean_data(self, comments_data: list[dict[str, str]]) -> list[dict[str, str]]:
+        return comments_data[:10000] if self.check_is_clean_is_needed() else comments_data
+        
+
+    
+class CommentLengthCleaner(Cleaner):
+    
+    def check_is_clean_is_needed(self, comments: str) -> bool:
+        return True if len(comments) >= 512 else False
+    
+    def clean_data(self):
+        raise NotImplementedError
+    
+
+    
+class VideoInfoCleaner(Cleaner):
+
+    def __init__(self, data) -> None:
+        super().__init__(data)
+
+    def check_is_clean_is_needed(self):
+        raise NotImplementedError
+    
+    def clean_data(self):
+        video_info = {
+            'thumbnail_url': self.data['items'][0]['snippet']['thumbnails']['high']['url'],
+            'video_title': self.data['items'][0]['snippet']['title'],
+            'channel_name': self.data['items'][0]['snippet']['channelTitle'],
+            'video_url': "youtu.be/"+self.data['items'][0]["id"],
+            'views': self.data['items'][0]['statistics']['viewCount'],
+            'likes': self.data['items'][0]['statistics']['likeCount'],
+            'published_date': self.data['items'][0]['snippet']['publishedAt'] 
+        }     
+
+        return video_info
+
+
+# Maybe can turn this into an abstract class?
+class CommentsCleaner:
+
+    def __init__(self, youtube_response_json) -> None:
+        self.youtube_response_json = youtube_response_json
+        self.comments_list_length_cleaner = CommentsListLengthCleaner()
+        self.comments_length_cleaner = CommentLengthCleaner()
+
     def extract_comment_text_and_date(self, youtube_response_json: list) -> list[dict[str, str]]:
-            '''Returns a json-like list with relevant data of the YouTube's response list.
-            Returns a list like this:
-                [
-                    {
-                        'text': comment['snippet']['topLevelComment']['snippet']['textDisplay'],
-                        'date': comment['snippet']['topLevelComment']['snippet']['updatedAt']
-                    },
-                    ...
-                ]
-            response_list: List with YouTube's response.'''
-
-            comments = [
+        '''Returns a list with relevant data of the 
+        YouTube's response list. Also, removes all 
+        the comments which text is longer than
+        512 characters, and removes all elements 
+        which index is longer than 10000.
+        Returns a list like this:
+            [
                 {
-                    'text': comment['snippet']['topLevelComment']['snippet']['textDisplay'], 
-                    'date': comment['snippet']['topLevelComment']['snippet']['updatedAt']
-                } 
-                for response in youtube_response_json for comment in response
-                ]
+                    'text': 'This is a comment',
+                    'date': '2023-11-24T03:57:00Z'
+                },
+                ...
+            ]
+        response_list: List with YouTube's response.'''
 
-            return comments
+        comments = [
+            {
+                'text': comment['snippet']['topLevelComment']['snippet']['textDisplay'], 
+                'date': comment['snippet']['topLevelComment']['snippet']['updatedAt']
+            } 
+            for response in youtube_response_json for comment in response 
+            if not self.comments_list_length_cleaner.check_is_clean_is_needed(comment['snippet']['topLevelComment']['snippet']['textDisplay'])
+            ]
+        
+        comments = self.comments_list_length_cleaner.clean_data(comments) if self.comments_list_length_cleaner.check_is_clean_is_needed() else comments
+
+        return comments
     
+class VideoInfoCleaner:
+
     def extract_video_relevant_info(self, video_info_response_json: dict) -> dict[str, str]:
+        '''Returns a '''
         video_info = {
             'thumbnail_url': video_info_response_json['items'][0]['snippet']['thumbnails']['high']['url'],
             'video_title': video_info_response_json['items'][0]['snippet']['title'],
@@ -136,31 +233,11 @@ class DataCleaner:
             'published_date': video_info_response_json['items'][0]['snippet']['publishedAt'] 
         }     
 
-        return video_info
-    
+        return video_info 
 
-    
-class CommentLengthChecker:
-
-    def check_if_comments_list_length_is_too_long(self, comments_data: list[dict[str, str]]) -> bool:
-        return True if len(comments_data) >= 10000 else False
-    
-    def check_if_comments_length_is_too_long(self, comment) -> bool:
-        return True if len(comment) >= 512 else False
-    
-    def clean_comments_list(self, comments_data: list[dict[str, str]]) -> list[dict[str, str]]:
-        if self.check_if_comments_list_length_is_too_long(comments_data):
-            return comments_data[:10000]
-        
-        else:
-            return comments_data
-
-    def clean_comments_too_long(self) -> list[dict[str, str]]:
-        pass
 
     
 class CommentAnalyzer:
-    '''This class works with'''
 
     processed = False
 
@@ -195,11 +272,14 @@ class CommentsOfVideoSentimentAnalyzer:
         self.video_id = video_id
 
     def fetch_youtube_video_comments_with_sentiments(self):
+        # Fetching all comments from a video.
         self.comments_data = self.youtube_connection.fetch_all_youtube_video_comments(self.video_id)
+
+        # Extracting only comment text and date (and cleaning comments if list is longer than 10000 comments or if comments are longer than 512 characters.)
         self.comments_data = self.data_cleaner.extract_comment_text_and_date(self.comments_data)
+        
+        # Adding sentiments to each comment registry.
         self.comments_analyzer.comments_data = self.comments_data
-        # Add length checks.
-        self.comments_analyzer.add_emotions_to_comments_data(self.comments_data)
         self.comments_data = self.comments_analyzer.get_comments_with_sentiment()
 
         self.processed = True
